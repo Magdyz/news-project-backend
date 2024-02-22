8f59656e06bbfc5672437a67630c474ab246a1c0
Magz <magdy_176@hotmail.com>
2024-02-22 11:13:44 +0000

    Get all users

--- app.js
+++ app.js
@@ -8,27 +8,29 @@ const {
   PostCommentRequest,
   patchArticle,
   removeCommentRequest,
+  getUsersRequest,
 } = require("./app.controller/app.controller");
 const {
   handle500ServerErrors,
-  handle404Errors,
   handleCustomErrors,
+  handlePSQLErrors,
 } = require("./ErrorHandlers/ErrorHandlers");
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/topics", getTopicsRequest);
 app.get("/api", getEndpointsRequest);
-app.get("/api/articles/:article_id", getArticleByIdRequest);
+app.get("/api/users", getUsersRequest);
+app.get("/api/topics", getTopicsRequest);
 app.get("/api/articles", getArticlesRequest);
+app.get("/api/articles/:article_id", getArticleByIdRequest);
 app.get("/api/articles/:id/comments", getCommentsByArticleId);
 app.post("/api/articles/:article_id/comments", PostCommentRequest);
 app.patch("/api/articles/:article_id", patchArticle);
 app.delete("/api/comments/:comment_id", removeCommentRequest);
 
+app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
-app.use(handle404Errors);
 app.use(handle500ServerErrors);
 
 module.exports = app;
