3da90af9798a6d3b4fe7c5ce9f824632c335356e
Magz <magdy_176@hotmail.com>
2024-02-22 14:03:06 +0000

    get articles by topic query

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
   patchArticle,
   removeCommentRequest,
   getUsersRequest,
+  getArticlesByTopic,
 } = require("./app.controller/app.controller");
 const {
   handle500ServerErrors,
@@ -20,9 +21,10 @@ const app = express();
 app.use(express.json());
 
 app.get("/api", getEndpointsRequest);
+app.get("/api/articles", getArticlesRequest);
+// app.get("/api/articles", getArticlesByTopic);
 app.get("/api/users", getUsersRequest);
 app.get("/api/topics", getTopicsRequest);
-app.get("/api/articles", getArticlesRequest);
 app.get("/api/articles/:article_id", getArticleByIdRequest);
 app.get("/api/articles/:id/comments", getCommentsByArticleId);
 app.post("/api/articles/:article_id/comments", PostCommentRequest);
